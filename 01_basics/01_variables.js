699782d572e2f6bd9dc0f68aa4bc3934c5bea3d0
NavneetNihal <lakranihal0070@gmail.com>
2025-11-06 10:34:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Nihal@gmail.com"
+var accountPassword = "nihal123"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "123nihu@gmail.com"
+accountPassword = "nih88y6"
+accountCity = "Ramgarh"
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
