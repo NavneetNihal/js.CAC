1ba0bbfbda749ba94024031cb96c2924d4446d10
NavneetNihal <lakranihal0070@gmail.com>
2025-11-06 10:35:22 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; /* treat all js code as newer version */
+
+// alert( 3 + 3 ) we are using node.js not browser 
+
+let name = "Nihal"
+let age = 18
+let isLoggedIn = "false"
+
+/* number => 2 to power 53
+   BigInt
+   string => ""
+   Boolean => true/false
+   null => standalone value 
+   undefined =>
+   symbol => unique */
+
+// object
+
+console.log(typeof "Nihal")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
